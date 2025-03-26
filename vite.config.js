3fd3800fc5cb465d638a2cafc50d56362594c6cc
Ale Roses <ale.vrs@outlook.com>
2025-03-25 20:52:49 -0500

    update configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/aleroses.github.io/"
-})
+  base: "/aleroses.github.io/",
+  esbuild: {
+    loader: "jsx",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+});
