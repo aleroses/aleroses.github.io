57266fe1056c65e4dabef609d5757c24dc6ca286
Ale Roses <ale.vrs@outlook.com>
2026-01-31 00:32:27 -0500

    update styles

--- src/components/background/BackgroundGalaxy.jsx
+++ src/components/background/BackgroundGalaxy.jsx
@@ -1,13 +1,13 @@
 export const BackgroundGalaxy = ({ children }) => {
   return (
-    <div className="relative min-h-screen bg-[#050810] overflow-hidden">
+    <div className="relative min-h-screen w-full overflow-hidden bg-[#050810]">
       <div className="stars stars-small"></div>
       <div className="stars stars-medium"></div>
       <div className="stars stars-big"></div>
       <div className="nebula"></div>
 
       {/* Contenido */}
-      <main className="relative z-10 min-h-screen grid place-content-center text-center text-white">
+      <main className="relative z-10 grid min-h-screen place-content-center p-4 text-center text-gray-50">
         {children}
       </main>
     </div>
